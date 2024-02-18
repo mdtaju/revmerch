0995d71b1dbf10c1d1fa8ed1472093e3f533f83d
Md Tajuuddin Tarek <61282753+mdtaju@users.noreply.github.com>
2024-02-18 12:59:19 +0600

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,31 @@
-;
+import Image from "next/image";
+import Map from "/public/assets/images/map.png";
+
 
 const About = () => {
       return (
-            <div>
-                  <h1>about</h1>
-            </div>
+            <main>
+                  <section className="container mx-auto py-[100px] px-4 md:px-0">
+                        <div className="bg-primary w-full p-4 md:p-8 rounded-[30px] md:rounded-[62px] grid grid-cols-1 md:grid-cols-2 gap-6">
+                              {/* map */}
+                              <div className="">
+                                    <Image
+                                          src={Map}
+                                          alt="address"
+                                          className="rounded-[37px]"
+                                    />
+                              </div>
+                              {/* info */}
+                              <div className="space-y-6 text-2xl font-bold">
+                                    <p>4 Street ibn fares,Sousse,Tunisia</p>
+                                    <p>+216 99999999</p>
+                                    <p>Instagram Page here</p>
+                                    <p>Tiktok Page here</p>
+                                    <p>Email here</p>
+                              </div>
+                        </div>
+                  </section>
+            </main>
       );
 };
 
